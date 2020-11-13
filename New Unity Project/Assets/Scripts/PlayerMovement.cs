c3cc28cdc14f55e2fd24783d86b922c330c8b70d
Milan Sietsma <milansietsma@gmail.com>
2020-11-13 12:14:08 +0100

    Added level 1 and player + movement

---
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int Speed = 10;
+    public float rotatespeed = 0.5f;
+
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+
+
+        transform.Rotate(0, Input.GetAxisRaw("Horizontal") * rotatespeed, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+    }
+}
